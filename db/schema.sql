2df20677be2a04842fd0b6a796e7e2ced4794d18
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-06-04 21:06:39 -0500

    found error in index

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,6 @@
 -- This is for when rebuilding the schema, that postgres has a db to switch off too to be able to select the correct db to drop
 \c postgres;
 
--- CREATE DATABASE
-DROP DATABASE IF EXISTS employee_db;
-
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
--- models/index.js
+++ models/index.js
@@ -25,7 +25,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'product_tag',
-  foreignKey: 'product_id',
+  foreignKey: 'tag_id',
   onDelete: 'CASCADE'
 })
 
